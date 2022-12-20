8128ef099ed8c2d163c4da04b148e07715a57b76
seop0914 <seop0914@gmail.com>
2022-12-21 00:21:17 +0900

    [Edit]post page

--- src/main/resources/static/javascript/post.js
+++ src/main/resources/static/javascript/post.js
@@ -9,11 +9,11 @@ window.onload = ()=>{
             if(hiddenCheck[i].checked === false) {
                 hiddenCheck[i].checked = true;
                 comments[i].style.display = "none";
-                hideCommentsButton[i].innerHTML = "댓글 모두 보기";
+                hideCommentsButton[i].innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"currentColor\" aria-hidden=\"true\" class=\"rotate-180 transform h-4 w-4\"><path fill-rule=\"evenodd\" d=\"M11.47 7.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5z\" clip-rule=\"evenodd\"></path></svg>댓글 모두 보기";
             } else {
                 hiddenCheck[i].checked = false;
                 comments[i].style.display = "block";
-                hideCommentsButton[i].innerHTML = "댓글 모두 숨기기";
+                hideCommentsButton[i].innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" fill=\"currentColor\" aria-hidden=\"true\" class=\"h-4 w-4\"><path fill-rule=\"evenodd\" d=\"M11.47 7.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5z\" clip-rule=\"evenodd\"></path></svg>댓글 모두 숨기기";
             }
         })
     }
