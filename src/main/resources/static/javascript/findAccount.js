7b4f52926ea7a377309f8299cee70ce6c9feb8b9
hahahoba <whgodlssla@gmail.com>
2022-12-27 13:53:35 +0900

    계정찾기 퍼블리싱 최종 완성

--- src/main/resources/static/javascript/findAccount.js
+++ src/main/resources/static/javascript/findAccount.js
@@ -12,13 +12,12 @@ window.onload = function () {
             document.getElementById("failed-search").style.display = "none";
             return false;
         } else { //이메일 형식이 맞을 경우
-            if(email == 0){ // DB와 비교해 있는 이메일이 아니면 출력
+            // DB와 비교해 있는 이메일이 아니면 출력
                 document.getElementById("not-email").style.display = "none";
                 document.getElementById("failed-search").style.display = "";
-            }
-            else{ // DB와 비교해 있는 이메일이면 확인 페이지로 넘어가기
 
-            }
+         // DB와 비교해 있는 이메일이면 확인 페이지로 넘어가기
+
         }
     }
 }
