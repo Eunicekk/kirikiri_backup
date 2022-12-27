787a00d5f2238fcddebec402caa68a7427a200dd
trainingkim <trainingkim30@gmail.com>
2022-12-27 17:18:01 +0900

    김연수  작업 완료

--- src/main/resources/static/javascript/passwordreset.js
+++ src/main/resources/static/javascript/passwordreset.js
@@ -1,24 +1,27 @@
 window.onload=function(){
-    let nowPass = document.getElementById("password1");
-    let resPass = document.getElementById("password2");
-    let retryPass = document.getElementById("password3");
-    let buttonEvent = document.getElementById("button-event");
+    let password1 = document.getElementById("password1");
+    let password2 = document.getElementById("password2");
+    let password3 = document.getElementById("password3");
+    let button_event= document.getElementById("button-event");
     // 비밀번호 최소 6자 이상 입력해주세요
     function over6Password() {
-        var reg = /^(?=.*?[a-z])(?=.*?[0-9]).{6,}$/;
-        var pw = $("#resPass").val();
-        if (false === reg.test(pw)) {
+        var condi = /^(?=.*?[a-z])(?=.*?[0-9]).{6,}$/;
+        var pwd = $("#resPass").val();
+        if (false === condi.test(pwd)) {
             alert('비밀번호는 6자 이상이어야 하며, 숫자/소문자를 포함해야 합니다.');
+            return false;
         } else {
             console.log("이벤트");
+            return true;
             }
         }
     }
     // 비밀번호가 일치하지 않습니다
-if(resPass!==retryPass) {
-    swal({
-        title: "비밀번호가 이상합니다.", text: "비밀번호가 일치하지 않습니다.", icon: "wornning"
-    })
+function confirmPassword(){
+    if(password2 != password3) {
+        password3.setCustomValidity("비밀번호가 일치하지 않습니다.");
+    } else {
+        password3.setCustomValidity('');
+    }
 }
-
-    // 비밀번호가 성공적으로 변경되었습니다. *이벤트*
+password3.onkeyup = confirmPassword;
